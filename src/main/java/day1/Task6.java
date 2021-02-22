79bf6f1978cbbabc06cce6416ed9b39309745abe
Professional <Everton1029>
2021-02-22 19:07:39 +0200

    change

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+    for (int x = 0 , k = 5, sum;x <= 9; x++){
+        sum = x * k;
+        System.out.println(x + " * " + k + " = " +  sum );
+    }
     }
 }
